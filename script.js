1c3bebb48deceabc4a11aafd995e51345dbb595c
Viktoria <wiktoriamitchell@yahoo.com>
2022-03-08 23:08:50 -0500

    console log buttons

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+const buttonNums = document.querySelectorAll(".btn");
 
-
+function getNumber() {
+    buttonNums.forEach((buttonNum) => {
+    buttonNum.addEventListener('click', () => {
+    const number = buttonNum.textContent;
+    console.log(number);
+});
+});
+}
+getNumber();
 
